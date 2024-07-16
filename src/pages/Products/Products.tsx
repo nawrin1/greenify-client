e1b62bf382eaca15fda5dd3c1ca33039b1d5d903
Jannatul Ferdous Nawrin <jannatulnawrin@gmail.com>
2024-07-16 20:55:39 +0600

    page refresh implemented condition handled

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -11,36 +11,104 @@ import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import { IoFilter } from "react-icons/io5";
 import { maxPrice, minPrice } from "../../redux/features/productSlice";
 import { Input } from "@mui/material";
+import { addProduct, total, updateCart } from "../../redux/features/cartSlice";
+import { toast } from "react-toastify";
 const Products = () => {
-  const  product  = useAppSelector((state) => state.product);
-  const dispatch=useAppDispatch()
-  const { data, isLoading, isError } = useGetAllProductsQuery( product, {
+  const product = useAppSelector((state) => state.product);
+  const dispatch = useAppDispatch();
+  const { cart } = useAppSelector((state) => state.cart);
+  const { data, isLoading, isError } = useGetAllProductsQuery(product, {
     pollingInterval: 1000,
   });
   // const { data,  isLoading,isError } =  useGetAllProductsQuery(undefined, {
   //   pollingInterval: 1000,
   // });
-// console.log(data,"from all products")
-if(isLoading || !data){
-  return <div className="min-h-screen flex justify-center items-center">
-    <ThreeDots
-  visible={true}
-  height="80"
-  width="80"
-  color="#4fa94d"
-  radius="9"
-  ariaLabel="three-dots-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  />
-  </div>
-}
-if(isError || !data){
-  return <div className="min-h-screen flex justify-center items-center">
-    <p>something went wrong</p>
-  </div>
-}
+  // console.log(data,"from all products")
+  if (isLoading || !data) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
+  }
+  if (isError || !data) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <p>something went wrong</p>
+      </div>
+    );
+  }
 
+  const clicked = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    data: Record<string, unknown>
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("clicked");
+
+    const product = cart.filter((eachProduct) => eachProduct._id == data._id);
+    // console.log(product)
+
+    if (product?.length == 0) {
+      dispatch(
+        addProduct({
+          _id: data._id as string,
+          title: data.title as string,
+          price: data.price as number,
+          quantity: 1,
+        })
+      );
+      dispatch(total(0));
+
+      return toast.success("Product Added!", {
+        style: {
+          fontFamily: "Cormorant Infant, sans-serif",
+          color: "black",
+          fontSize: "20px",
+        },
+      });
+    } else {
+      cart.filter((each) => {
+        if (each._id == data?._id) {
+          // console.log("enter")
+
+          const newValue = each.quantity + 1;
+          console.log(each, newValue, data.quantity);
+          if (newValue > (data.quantity as number)) {
+            return toast.error("Quantity exceeded", {
+              style: {
+                fontFamily: "Cormorant Infant, sans-serif",
+                color: "black",
+                fontSize: "20px",
+              },
+            });
+          } else {
+            dispatch(updateCart(data as Record<string, unknown>));
+            dispatch(total(0));
+            return toast.success("Product Added!", {
+              style: {
+                fontFamily: "Cormorant Infant, sans-serif",
+                color: "black",
+                fontSize: "20px",
+              },
+            });
+          }
+        }
+      });
+    }
+
+    // event.stopPropagation();
+  };
 
   return (
     <div className="prod pb-36 flex flex-col items-center px-4 relative">
@@ -64,68 +132,109 @@ if(isError || !data){
         </div>
       </div>
       <div className="flex justify-end w-full  ">
-      <div className="dropdown dropdown-left dropdown-end pt-4">
-      <div tabIndex={0} role="button" className=" bg-slate-200 hover:bg-black hover:text-white rounded-lg w-[100px] h-[35px] px-4 py-2 m-1 text-xl flex items-center"><p>Filter</p> <div className="text-2xl  pl-2"><IoFilter /></div></div>
-      <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-lg w-32 p-2 shadow">
-        <li ><Input onChange={(event)=>dispatch(minPrice(parseInt(event?.target.value)))}placeholder="min Price" /></li>
-        <li ><Input onChange={(event)=>dispatch(maxPrice(parseInt(event?.target.value)))}placeholder="max Price" /></li>
-       
-       
-      </ul>
-    </div>
+        <div className="dropdown dropdown-left dropdown-end pt-4">
+          <div
+            tabIndex={0}
+            role="button"
+            className=" bg-slate-200 hover:bg-black hover:text-white rounded-lg w-[100px] h-[35px] px-4 py-2 m-1 text-xl flex items-center"
+          >
+            <p>Filter</p>{" "}
+            <div className="text-2xl  pl-2">
+              <IoFilter />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-lg w-32 p-2 shadow"
+          >
+            <li>
+              <Input
+                onChange={(event) =>
+                  dispatch(minPrice(parseInt(event?.target.value)))
+                }
+                placeholder="min Price"
+              />
+            </li>
+            <li>
+              <Input
+                onChange={(event) =>
+                  dispatch(maxPrice(parseInt(event?.target.value)))
+                }
+                placeholder="max Price"
+              />
+            </li>
+          </ul>
         </div>
+      </div>
 
       <div className="w-[70%]  grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center mx-auto place-items-center mt-20 pb-20">
-            {
-                data.data.slice(0,6).map((product,idx)=><Link to={`/products/${product.title}`}><div key={idx} className="card  bg-slate-500   ">
-                <div className="absolute flex justify-end w-full">
-                  <div className="shop w-[40px] h-[40px] bg-[#d4d4d4] rounded-full flex  justify-center items-center">
-                  <p className="text-black text-2xl"><CgShoppingCart /></p>
-                  </div>
+        {data.data.slice(0, 6).map((product, idx) => (
+          <Link to={`/products/${product.title}`}>
+            <div key={idx} className="card  bg-slate-500   ">
+              <div className="absolute flex justify-end w-full">
+                <div
+                  onClick={(event) => clicked(event,product)}
+                  className="shop w-[40px] h-[40px] bg-[#d4d4d4] rounded-full flex  justify-center items-center"
+                >
+                  <p className="text-black text-2xl">
+                    <CgShoppingCart />
+                  </p>
                 </div>
-                <img className="h-[350px] w-[250px] object-fill" src={product.image} alt="" />
-                <div className="intro">
-                  <div className="flex justify-between">
-                    <div className="">
-                     <div>
-                     <h1 className="text-[22px] text-wrap overflow-hidden">{product.title} </h1>
-                     <div className="flex ">
-                     <p className="text-2xl">{product.rating}  </p>
-                     <div className="text-yellow-300 text-xl mt-1"><IoMdStar /></div>
-                     </div>
-
-                     </div>
-                      {/* <div>
+              </div>
+              <img
+                className="h-[350px] w-[250px] object-fill"
+                src={product.image}
+                alt=""
+              />
+              <div className="intro">
+                <div className="flex justify-between">
+                  <div className="">
+                    <div>
+                      <h1 className="text-[22px] text-wrap overflow-hidden">
+                        {product.title}{" "}
+                      </h1>
+                      <div className="flex ">
+                        <p className="text-2xl">{product.rating} </p>
+                        <div className="text-yellow-300 text-xl mt-1">
+                          <IoMdStar />
+                        </div>
+                      </div>
+                    </div>
+                    {/* <div>
                       <p className="text-2xl mt-0">{product.rating}</p>
                       </div> */}
-                    </div>
-                    <div className="flex">
-                      <p className="text-xl ">{product.price}</p>
-                      <div className=" text-xl mt-1"><LiaDollarSignSolid /></div>
-                      
+                  </div>
+                  <div className="flex">
+                    <p className="text-xl ">{product.price}</p>
+                    <div className=" text-xl mt-1">
+                      <LiaDollarSignSolid />
                     </div>
                   </div>
-                  <p className="para text-xl">Category: {product.category}</p>
-                  <p className="para text-[16px] text-[#f3f3f3] overflow-ellipsis">
-                   {product.description}
-                  </p>
-                  <p></p>
                 </div>
-              </div></Link>)
-            }
-            
-        </div>
+                <p className="para text-xl">Category: {product.category}</p>
+                <p className="para text-[16px] text-[#f3f3f3] overflow-ellipsis">
+                  {product.description}
+                </p>
+                <p></p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
 
-<NavLink to='/products'>      <div className="button border-[#bdc468] border-2 w-[180px] h-[50px] flex rounded-[50px] relative  mt-6">
-        <div className="bg-[#2b3c17] w-[100%] h-[50px]  rounded-l-[50px] flex justify-center items-center pr-2">
-          <p className="text-xl text-[#bdc468]">See More</p>
-        </div>
-        <div className="white bg-white w-[30%] h-[50px]  rounded-[50px] absolute z-10 right-0 top-0 bottom-0 flex justify-center items-center">
-          <div className="text-[18px] text-[#2b3c17]">
-            <FaArrowRight />
+      <NavLink to="/products">
+        {" "}
+        <div className="button border-[#bdc468] border-2 w-[180px] h-[50px] flex rounded-[50px] relative  mt-6">
+          <div className="bg-[#2b3c17] w-[100%] h-[50px]  rounded-l-[50px] flex justify-center items-center pr-2">
+            <p className="text-xl text-[#bdc468]">See More</p>
+          </div>
+          <div className="white bg-white w-[30%] h-[50px]  rounded-[50px] absolute z-10 right-0 top-0 bottom-0 flex justify-center items-center">
+            <div className="text-[18px] text-[#2b3c17]">
+              <FaArrowRight />
+            </div>
           </div>
         </div>
-      </div></NavLink>
+      </NavLink>
     </div>
   );
 };
--- src/pages/shared/Navbar.tsx
+++ src/pages/shared/Navbar.tsx
@@ -7,16 +7,25 @@ import { NavLink } from 'react-router-dom';
 import './Navbar.css'
 import { useAppDispatch, useAppSelector } from '../../redux/hook';
 import { search } from '../../redux/features/productSlice';
+import useBeforeUnload from '../../components/hooks/reload';
 
 const Navbar = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isCart,setCart]=useState(false)
     const dispatch=useAppDispatch()
     const {total}=useAppSelector((state) => state.cart)
+    const {cart}=useAppSelector((state) => state.cart)
+    console.log(cart)
     console.log(total,"from nav")
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
+    const cartToggle=()=>{
+        setCart(!isCart)
+    }
+    const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0)
+    useBeforeUnload(cart.length !== 0);
 
     return (
         <div className="w-full sticky top-0 z-50">
@@ -54,7 +63,7 @@ const Navbar = () => {
                             <Input onChange={(event)=>dispatch(search(event?.target.value))}placeholder="Search" />
                         </div>
                         {/* Cart Dropdown */}
-                        <div className="dropdown dropdown-end">
+                        {/* <div className="dropdown dropdown-end">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                                 <div className="indicator">
                                     <svg
@@ -81,14 +90,95 @@ const Navbar = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
+                         <div>
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" onClick={cartToggle}>
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0 2 2 0 014 0z"
+            />
+          </svg>
+          <span className="badge badge-sm indicator-item">{total}</span>
+        </div>
+      </div>
+
+      <div className={`fixed inset-y-0 right-0 bg-white w-80 z-50 transition-transform duration-300 ${isCart ? 'translate-x-0' : 'translate-x-full'}`}>
+        <div className="text-[16px] pt-2 px-2  w-full flex justify-end" onClick={cartToggle}>
+          <div>
+          <ImCross />
+          </div>
+        </div>
+        {/* cart div */}
+        <div className='px-4   h-full '>
+        <div className='flex justify-center  '>
+                <h2 className='text-2xl font-semibold text-[#9ac284] text-center pt-7 pb-3'>Shopping Cart</h2>
+                <div className="text-[16px] pt-9"><FaLeaf /></div>
+                </div>
+        {cart.length > 0 ? 
+            <div className=' flex flex-col justify-between' >
+
+
+            
+            {cart.map((item, index) => (
+              <div key={index} className=" pb-2">
+                <div className="flex justify-between text-[16px] border-b border-gray-200 text-[#2b3c17] w-full">
+                  
+                  <div className='flex-4/6  w-4/6 '>{index+1}. {item.title.slice(0,20)}</div>
+                  <div className='flex-1/6  w-1/6 text-center'>x {item.quantity}</div>
+                  <div className='flex-1/6  w-1/6 text-center'>${item.price}</div>
+                </div>
+
+
+              </div>
+            ))}
+            <div className=''>
+                    <p className='text-[20px] text-[#6f8d5f] pt-10'>Total Price:  <span className='text-black'>${totalPrice}</span></p>
+
+                </div>
+                <div>
+                <button className="relative shadow-sm shadow-slate-600 rounded-sm w-full h-[40px] overflow-hidden border border-black group-hover:border-white text-black bg-white transition-all duration-500 ease-out group">
+  <span className="absolute inset-0 w-full h-full bg-black transform translate-x-full transition-transform duration-500 ease-out group-hover:translate-x-0"></span>
+  <span className="relative z-10 flex items-center justify-center h-full text-black transition-colors duration-500 ease-out group-hover:text-white lg:text-xl text-[15px]">CheckOut</span>
+</button>
+                </div>
+            </div>
+            
+           : (
+            <li className='text-xl '>Cart is empty!</li>
+          )}
+            
+
+
+        </div>
+
+
+      </div>
+    </div>
+
+
+
+
+
+
                     </div>
                 </div>
             </div>
 
 
             
-            <div className={`fixed inset-0 bg-gray-900 bg-opacity-50 z-50 lg:hidden transition-opacity duration-300 ${isSidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={toggleSidebar}></div>
+            <div className={`fixed inset-0 bg-gray-900 bg-opacity-50 z-50 lg:hidden transition-opacity duration-300 ${isSidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={toggleSidebar}>
+
+            </div>
             <div className={`fixed inset-y-0 left-0 bg-white w-64 z-50 transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
             <div className={`text-2xl pt-4 px-2 lg:hidden  w-[50px]`} onClick={toggleSidebar}>
             <ImCross />
